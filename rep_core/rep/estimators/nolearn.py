bfe9a548d0157481f624f927eb4ded75aae0b16c
Алексей Бердников <alexey-berdnikov@yandex.ru>
2015-03-12 00:30:17 +0300

    NolearnClassifier.__init__() changed

--- rep_core/rep/estimators/nolearn.py
+++ rep_core/rep/estimators/nolearn.py
@@ -6,12 +6,14 @@ from abc import ABCMeta
 from .interface import Classifier, Regressor
 from .utils import check_inputs
 
+from nolearn.dbn import DBN
+
 __author__ = 'Alexey Berdnikov'
 
 class NolearnClassifier(Classifier):
-    def __init__(self, clf, features=None):
-        # Что, если clf.layer_sizes[0] не равно len(features)?
-        self.clf = clf
+    def __init__(self, features=None, layer_sizes=None, learn_rates=0.1, learn_rate_decays=1.0, epochs=10, verbose=0):
+        # Что, если layer_sizes[0] не равно len(features)?
+        self.clf = DBN(layer_sizes=layer_sizes, learn_rates=learn_rates, epochs=epochs, verbose=verbose)
         Classifier.__init__(self, features=features)
 
     def fit(self, X, y, sample_weight=None):
@@ -33,4 +35,3 @@ class NolearnClassifier(Classifier):
         if sample_weight is not None:
             raise ValueError("The sample_weight parameter is not supported for nolearn")
         return self.clf.score(self._get_train_features(X).values, y)
-
