c4cf21dc07edc46678ce3de42fd7f23f5dc8a913
haskile <haskile@gmail.com>
2015-03-26 00:03:01 +0300

    Fixes in TheanetsClassifier

--- rep_core/rep/estimators/theanets.py
+++ rep_core/rep/estimators/theanets.py
@@ -4,21 +4,19 @@ import numpy
 from .interface import Classifier
 from .utils import check_inputs
 from sklearn.preprocessing import MinMaxScaler, Imputer
+import os
+import tempfile
 
 try:
     import theanets as tnt
 except ImportError as e:
     raise ImportError("Install theanets before")
 
-__author__ = 'Ignatyeva Lisa'
-
-# TODO: make printing TheanetsClassifier informative (include training method & its parameters)
-# TODO: input and output layers' sizes can be pulled from the dataset automatically
-#       and what should we do if, pulling it automatically, we get a dataset with another output size, which is
-#       quite possible?
+__author__ = 'Lisa Ignatyeva'
 
 
 class TheanetsClassifier(Classifier):
+    # TODO: fix the doc
     """
     Implements classification from Theanets library.
 
@@ -49,46 +47,55 @@ class TheanetsClassifier(Classifier):
     :type decode_from: positive int
     :param features: list of features to train model
     :type features: None or list(str)
-    :param dict kwargs: parameters to specify training algorithm
+    :param list(dict) or None trainers: parameters to specify training algorithm
     """
     def __init__(self, 
                  layers,
-                 hidden_activation=None,
-                 output_activation=None,
+                 hidden_activation='logistic',
+                 output_activation='linear',
                  rng=None,
-                 input_noise=None,
-                 hidden_noise=None,
-                 input_dropouts=None,
-                 hidden_dropouts=None,
-                 decode_from=None,
+                 input_noise=0,
+                 hidden_noise=0,
+                 input_dropouts=0,
+                 hidden_dropouts=0,
+                 decode_from=1,
                  features=None,
-                 **kwargs):
-        self.network_params_names = {'hidden_activation', 'output_activation', 'rng',
-                                     'input_noise', 'hidden_noise', 'input_dropouts',
-                                     'hidden_dropouts', 'decode_from'}
+                 trainers=None):
         self.layers = layers
-        self.network_params = {}
-        # TODO: rewrite these ifs into something prettier
-        if hidden_activation is not None:
-            self.network_params['hidden_activation'] = hidden_activation
-        if output_activation is not None:
-            self.network_params['output_activation'] = output_activation
-        # TODO: actually, None is a valid value for rng to be passed...
-        if rng is not None:
-            self.network_params['rng'] = rng
-        if input_noise is not None:
-            self.network_params['input_noise'] = input_noise
-        if hidden_noise is not None:
-            self.network_params['hidden_noise'] = hidden_noise
-        if input_dropouts is not None:
-            self.network_params['input_dropouts'] = input_dropouts
-        if hidden_dropouts is not None:
-            self.network_params['hidden_dropouts'] = hidden_dropouts
-        if decode_from is not None:
-            self.network_params['decode_from'] = decode_from
-        self.optimize_params = kwargs
+        self.network_params = {'hidden_activation': hidden_activation, 'output_activation': output_activation,
+                               'rng': rng, 'input_noise': input_noise, 'hidden_noise': hidden_noise,
+                               'input_dropouts': input_dropouts, 'hidden_dropouts': hidden_dropouts,
+                               'decode_from': decode_from}
+        # TODO: Do something with rng!
+        self.trainers = trainers  # should it really be None?
+        if self.trainers is None:
+            self.trainers = [{}]
         self.exp = None
         Classifier.__init__(self, features=features)
+
+    def __getstate__(self):
+        result = self.__dict__.copy()
+        del result['exp']
+        if self.exp is None:
+            result['dumped_exp'] = None
+        else:
+            with tempfile.NamedTemporaryFile() as dump:
+                self.exp.save(dump.name)
+                with open(dump.name, 'rb') as dumpfile:
+                    result['dumped_exp'] = dumpfile.read()
+        return result
+
+    def __setstate__(self, dictionary):
+        self.__dict__ = dictionary
+        if dictionary['dumped_exp'] is None:
+            self.exp = None
+        else:
+            with tempfile.NamedTemporaryFile() as dump:
+                with open(dump.name, 'wb') as dumpfile:
+                    dumpfile.write(dictionary['dumped_exp'])
+                assert os.path.exists(dump.name), 'there is no such file: {}'.format(dump.name)
+                self.exp = tnt.Experiment.load(dump.name)
+        del dictionary['dumped_exp']
 
     def set_params(self, **params):
         """
@@ -97,75 +104,84 @@ class TheanetsClassifier(Classifier):
         :param dict params: parameters to set in model
         """
         for key, value in params.items():
-            if key in self.network_params_names:
-                self.network_params[key] = value
+            if hasattr(self, key):
+                setattr(self, key, value)
             else:
-                self.optimize_params[key] = value
+                if key in self.network_params:
+                    self.network_params[key] = value
+                else:
+                    # TODO: if there is only one trainer, parameters names should be allowed to be simpler
+                    trainer_num, sep, param = key.partition('_')
+                    if not sep:
+                        raise AttributeError(key + ' is an invalid parameter for a NN with multiple training')
+                    trainer_num = int(trainer_num[7:])
+                    # resize if needed
+                    self.trainers[trainer_num][param] = value
 
-    def get_params(self):
+    def get_params(self, deep=True):
         """
         Get parameters of this estimator
 
-        :return dict which holds:
-            'optimize_params': dict or (if additional_fit was called at least once) list(dict)
-                each subdict here contains all the parameters which were used in a fit/additional_fit call
-            'network_params': a dict with network parameters
-            'features': a list of features used (None if all were used).
+        :return dict
         """
-        parameters = {'optimize_params': self.optimize_params,
+        parameters = {'layers': self.layers,
                       'network_params': self.network_params,
-                      'features': self.features}
+                      'trainers': self.trainers,
+                      'features': self.features,
+                      'exp': self.exp}
         return parameters
 
     def _transform_data(self, data):
         return MinMaxScaler().fit_transform(Imputer().fit_transform(self._get_train_features(data)))
 
+    def _check_fitted(self):
+        assert self.exp is not None, 'Classifier wasn`t fitted, please call `fit` first'
 
     def fit(self, X, y, sample_weight=None):
         """
-        Train the classifier
+        Train the classifier from scratch.
 
         :param pandas.DataFrame X: data shape [n_samples, n_features]
         :param y: values - array-like of shape [n_samples]
         :param sample_weight: weight of events,
                array-like of shape [n_samples] or None if all weights are equal
+        :return: self
+        """
+        self.exp = None
+        for trainer in self.trainers:
+            self.partial_fit(X, y, new_trainer=False, **trainer)
+        return self
+
+    def partial_fit(self, X, y, sample_weight=None, new_trainer=True,  **trainer):
+        """
+        Train the classifier by training the existing classifier again.
+
+        :param pandas.DataFrame X: data shape [n_samples, n_features]
+        :param y: values - array-like of shape [n_samples]
+        :param sample_weight: weight of events,
+               array-like of shape [n_samples] or None if all weights are equal
+        :param bool new_trainer: True if the trainer is not stored in self.trainers
+        :param dict trainer: parameters of the training algorithm we want to use now
         :return: self
         """
         if sample_weight is not None:
-            raise NotImplementedError('sample_weight is not supported yet for theanets')
+            # https://github.com/lmjohns3/theanets/issues/58
+            raise NotImplementedError('sample_weight is not supported for theanets')
+        X, y, sample_weight = check_inputs(X, y, sample_weight)
+        X = self._transform_data(X)
         self.classes_ = numpy.unique(y)
-        X, y, sample_weight = check_inputs(X, y, sample_weight)
-        X = self._transform_data(X)
-        if self.layers[0] == -1:
-            self.layers = (X.shape[1],) + self.layers[1:]
-        if self.layers[-1] == -1:
-            self.layers = self.layers[:-1] + (len(self.classes_),)
-        self.exp = tnt.Experiment(tnt.Classifier, layers=self.layers, **self.network_params)
-
-        self.exp.train((X.astype(numpy.float32), y.astype(numpy.int32)),
-                       **self.optimize_params)
-
-    def additional_fit(self, X, y, sample_weight=None, **kwargs):
-        """
-        Train the classifier again
-
-        :param pandas.DataFrame X: data shape [n_samples, n_features]
-        :param y: values - array-like of shape [n_samples]
-        :param sample_weight: weight of events,
-               array-like of shape [n_samples] or None if all weights are equal
-        :param dict kwargs: parameters of the training algorithm we want to use now
-        :return: self
-        """
         if self.exp is None:
-            raise AttributeError('you cannot call additional_fit before calling fit')
-        if isinstance(self.optimize_params, list):
-            self.optimize_params.append(kwargs)
-        else:
-            self.optimize_params = [self.optimize_params, kwargs]
-        X, y, sample_weight = check_inputs(X, y, sample_weight)
-        X = self._transform_data(X)
+            # initialize experiment
+            if self.layers[0] == -1:
+                self.layers = (X.shape[1],) + tuple(self.layers[1:])
+            if self.layers[-1] == -1:
+                self.layers = self.layers[:-1] + (len(self.classes_),)
+            self.exp = tnt.Experiment(tnt.Classifier, layers=self.layers, **self.network_params)
+        if new_trainer:
+            self.trainers.append(trainer)
         self.exp.train((X.astype(numpy.float32), y.astype(numpy.int32)),
-                       **kwargs)
+                       **trainer)
+        return self
 
     def predict_proba(self, X):
         """
@@ -174,6 +190,7 @@ class TheanetsClassifier(Classifier):
         :param pandas.DataFrame X: data shape [n_samples, n_features]
         :rtype: numpy.array of shape [n_samples, n_classes] with probabilities
         """
+        self._check_fitted()
         X = self._transform_data(X)
         return self.exp.network.predict(X.astype(numpy.float32))
 
@@ -184,4 +201,4 @@ class TheanetsClassifier(Classifier):
         :param pandas.DataFrame X: data shape [n_samples, n_features]
         :return: iterator
         """
-        raise NotImplementedError('staged_predict_proba is not supported yet for theanets')
+        raise NotImplementedError('staged_predict_proba is not supported for theanets')
