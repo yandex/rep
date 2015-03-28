e5794e6a1cc2ae66cfa0417bf224db4b5ade6cb7
quadrocube <gliderok@gmail.com>
2015-03-29 00:33:33 +0300

    More testing over here

--- tests/test_neurolab.py
+++ tests/test_neurolab.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, absolute_import
 from ._test_classifier import check_classifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
 from rep.estimators.sklearn import SklearnClassifier
 from rep.estimators.neurolab import NeurolabClassifier
 import neurolab as nl
@@ -11,16 +11,18 @@ __author__ = 'Sterzhanov Vladislav'
 def test_neurolab_single_classification():
     f = nl.trans.LogSig()
     f2 = nl.trans.SoftMax()
-    check_classifier(NeurolabClassifier(show=0, layers=[], epochs=400, trainf=nl.train.train_rprop),
+    check_classifier(NeurolabClassifier(show=0, layers=[], epochs=50, trainf=nl.train.train_rprop),
+                     supports_weight=False, has_staged_pp=False, has_importances=False)
+    check_classifier(NeurolabClassifier(net_type='single-layer', cn='auto', show=0, epochs=50, trainf=nl.train.train_delta),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
 
 
-#def test_neurolab_multiple_classification():
-#    check_classifier(NeurolabClassifier(show=0, layers=[], epochs=400, trainf=nl.train.train_rprop),
-#                     supports_weight=False, has_staged_pp=False, has_importances=False)
+def test_neurolab_multiple_classification():
+    check_classifier(NeurolabClassifier(show=0, layers=[], epochs=50, trainf=nl.train.train_rprop),
+                     supports_weight=False, has_staged_pp=False, has_importances=False)
 
 
-#def test_neurolab_stacking():
-#    base_nlab = NeurolabClassifier(show=0, layers=[], epochs=400, trainf=nl.train.train_rprop)
-#    check_classifier(SklearnClassifier(clf=AdaBoostClassifier(base_estimator=base_nlab, n_estimators=3)),
-#                     has_staged_pp=False)
+def test_neurolab_stacking():
+    base_nlab = NeurolabClassifier(show=0, layers=[], epochs=50, trainf=nl.train.train_rprop)
+    check_classifier(SklearnClassifier(clf=BaggingClassifier(base_estimator=base_nlab, n_estimators=3)),
+                     supports_weight=False, has_staged_pp=False, has_importances=False)
