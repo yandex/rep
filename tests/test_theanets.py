cae19b28b52be2d6bff6796253ac9277c2af0631
haskile <haskile@gmail.com>
2015-03-28 23:24:27 +0300

    more fixes in TheanetsClassifier

--- tests/test_theanets.py
+++ tests/test_theanets.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function, absolute_import
 from ._test_classifier import check_classifier
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
 from rep.estimators.sklearn import SklearnClassifier
 from rep.estimators.theanets import TheanetsClassifier
 
@@ -8,21 +8,19 @@ __author__ = 'Lisa Ignatyeva'
 
 
 def test_theanets_single_classification():
-    check_classifier(TheanetsClassifier(layers=[-1, 10, -1]),
+    check_classifier(TheanetsClassifier(),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
-    check_classifier(TheanetsClassifier(layers=[-1, 10, -1], [{'optimize': 'sgd', 'learning_rate': 0.3}]),
+    check_classifier(TheanetsClassifier(layers=[20], trainers=[{'optimize': 'sgd', 'learning_rate': 0.3}]),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
 
 
 def test_theanets_multiple_classification():
-    check_classifier(TheanetsClassifier(layers=[-1, 10, -1], [{'optimize': 'pretrain'}, {'optimize': 'nag'}]),
+    check_classifier(TheanetsClassifier(trainers=[{'optimize': 'pretrain'}, {'optimize': 'nag'}]),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
 
 
 def test_simple_stacking_theanets():
-    base_tnt = TheanetsClassifier(layers=[-1, 10, -1])
-    check_classifier(SklearnClassifier(clf=AdaBoostClassifier(base_estimator=base_tnt, n_estimators=3)),
-                     has_staged_pp=False)
-
-
+    base_tnt = TheanetsClassifier()
+    check_classifier(SklearnClassifier(clf=BaggingClassifier(base_estimator=base_tnt, n_estimators=3)),
+                     supports_weight=False, has_staged_pp=False, has_importances=False)
 
