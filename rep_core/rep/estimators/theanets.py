fe0f770c55133339dc71cc91838cec6d0211a70b
haskile <haskile@gmail.com>
2015-04-10 10:59:29 +0300

    theanets base & theanets regressor basics added

--- rep_core/rep/estimators/theanets.py
+++ rep_core/rep/estimators/theanets.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function, absolute_import
 import numpy
-
-from .interface import Classifier
+from abc import abstractmethod
+from .interface import Classifier, Regressor
 from .utils import check_inputs
 from sklearn.preprocessing import MinMaxScaler, Imputer
 from sklearn.utils import check_random_state
@@ -16,54 +16,24 @@ except ImportError as e:
 __author__ = 'Lisa Ignatyeva'
 
 UNSUPPORTED_OPTIMIZERS = ['pretrain', 'sample', 'hf']
+# pretrain and sample data formats are too different from what we support here
+# hf now does not work in theanets, see https://github.com/lmjohns3/theanets/issues/62
 
-
-class TheanetsClassifier(Classifier):
-    """
-    Implements classification from Theanets library.
-
-    Parameters:
-    -----------
-    :param layers: A sequence of values specifying the hidden layer configuration for the network. For more information
-        please see 'Specifying layers' in theanets documentation:
-        http://theanets.readthedocs.org/en/latest/creating.html#creating-specifying-layers
-        Note that theanets "layers" parameter included input and output layers in the sequence as well.
-    :type layers: sequence of int, tuple, dict
-    :param int input_layer: size of the input layer. If equals -1, the size is taken from the training dataset.
-    :param int output_layer: size of the output layer. If equals -1, the size is taken from the training dataset.
-    :param str hidden_activation: the name of an activation function to use on hidden network layers by default.
-    :param str output_activation: The name of an activation function to use on the output layer by default.
-    :param int random_state: random seed
-    :param float input_noise: Standard deviation of desired noise to inject into input.
-    :param float hidden_noise: Standard deviation of desired noise to inject into hidden unit activation output.
-    :param input_dropouts: Proportion of input units to randomly set to 0.
-    :type input_dropouts: float in [0, 1]
-    :param hidden_dropouts: Proportion of hidden unit activations to randomly set to 0.
-    :type hidden_dropouts: float in [0, 1]
-    :param decode_from: Any of the hidden layers can be tapped at the output. Just specify a value greater than
-        1 to tap the last N hidden layers. The default is 1, which decodes from just the last layer.
-    :type decode_from: positive int
-    :param scaler: scaler used to transform data
-    :type scaler: scaler from sklearn.preprocessing or None
-    :param features: list of features to train model
-    :type features: None or list(str)
-    :param list(dict) or None trainers: parameters to specify training algorithm
-    """
-    def __init__(self, 
-                 layers=(10,),
-                 input_layer=-1,
-                 output_layer=-1,
-                 hidden_activation='logistic',
-                 output_activation='linear',
-                 random_state=42,
-                 input_noise=0,
-                 hidden_noise=0,
-                 input_dropouts=0,
-                 hidden_dropouts=0,
-                 decode_from=1,
-                 scaler=MinMaxScaler(),
-                 features=None,
-                 trainers=None):
+class TheanetsBase(object):
+    def __init__(self,
+                 layers,
+                 input_layer,
+                 output_layer,
+                 hidden_activation,
+                 output_activation,
+                 random_state,
+                 input_noise,
+                 hidden_noise,
+                 input_dropouts,
+                 hidden_dropouts,
+                 decode_from,
+                 scaler,
+                 trainers):
         self.layers = list(layers)
         self.input_layer = input_layer
         self.output_layer = output_layer
@@ -73,11 +43,11 @@ class TheanetsClassifier(Classifier):
                                'input_dropouts': input_dropouts, 'hidden_dropouts': hidden_dropouts,
                                'decode_from': decode_from}
         self.scaler = scaler
+        self.imputer = Imputer()
         self.trainers = trainers
         if self.trainers is None:
             self.trainers = [{}]
         self.exp = None
-        Classifier.__init__(self, features=features)
 
     def __getstate__(self):
         """
@@ -156,19 +126,24 @@ class TheanetsClassifier(Classifier):
         return parameters
 
     def _transform_data(self, data):
-        data = Imputer().fit_transform(self._get_train_features(data, allow_nans=True))
+        data_backup = data
+        data_backup = (self._get_train_features(data_backup, allow_nans=True))
         if self.scaler is None:
-            return data
+            if self._is_fitted():
+                return self.imputer.transfrom(data_backup)
+            else:
+                return self.imputer.fit_transform(data_backup)
+
         if self._is_fitted():
-            return self.scaler.transform(data)
-        return self.scaler.fit_transform(data)
+            return self.scaler.transform(self.imputer.transfrom(data_backup))
+        return self.scaler.fit_transform(self.imputer.fit_transform(data_backup))
 
     def _is_fitted(self):
         return self.exp is not None
 
     def fit(self, X, y, sample_weight=None):
         """
-        Train the classifier from scratch.
+        Train the estimator from scratch.
 
         :param pandas.DataFrame X: data shape [n_samples, n_features]
         :param y: values - array-like of shape [n_samples]
@@ -183,6 +158,85 @@ class TheanetsClassifier(Classifier):
                     raise NotImplementedError(optimizer + ' is not supported')
             self.partial_fit(X, y, new_trainer=False, **trainer)
         return self
+
+    @abstractmethod
+    def partial_fit(self, X, y, sample_weight=None, new_trainer=True,  **trainer):
+        """
+        Train the estimator by training the existing classifier again.
+
+        :param pandas.DataFrame X: data shape [n_samples, n_features]
+        :param y: values - array-like of shape [n_samples]
+        :param sample_weight: weight of events,
+               array-like of shape [n_samples] or None if all weights are equal
+        :param bool new_trainer: True if the trainer is not stored in self.trainers
+        :param dict trainer: parameters of the training algorithm we want to use now
+        :return: self
+        """
+        pass
+
+
+
+
+class TheanetsClassifier(TheanetsBase, Classifier):
+    """
+    Implements classification from Theanets library.
+
+    Parameters:
+    -----------
+    :param layers: A sequence of values specifying the hidden layer configuration for the network. For more information
+        please see 'Specifying layers' in theanets documentation:
+        http://theanets.readthedocs.org/en/latest/creating.html#creating-specifying-layers
+        Note that theanets "layers" parameter included input and output layers in the sequence as well.
+    :type layers: sequence of int, tuple, dict
+    :param int input_layer: size of the input layer. If equals -1, the size is taken from the training dataset.
+    :param int output_layer: size of the output layer. If equals -1, the size is taken from the training dataset.
+    :param str hidden_activation: the name of an activation function to use on hidden network layers by default.
+    :param str output_activation: The name of an activation function to use on the output layer by default.
+    :param int random_state: random seed
+    :param float input_noise: Standard deviation of desired noise to inject into input.
+    :param float hidden_noise: Standard deviation of desired noise to inject into hidden unit activation output.
+    :param input_dropouts: Proportion of input units to randomly set to 0.
+    :type input_dropouts: float in [0, 1]
+    :param hidden_dropouts: Proportion of hidden unit activations to randomly set to 0.
+    :type hidden_dropouts: float in [0, 1]
+    :param decode_from: Any of the hidden layers can be tapped at the output. Just specify a value greater than
+        1 to tap the last N hidden layers. The default is 1, which decodes from just the last layer.
+    :type decode_from: positive int
+    :param scaler: scaler used to transform data
+    :type scaler: scaler from sklearn.preprocessing or None
+    :param features: list of features to train model
+    :type features: None or list(str)
+    :param list(dict) or None trainers: parameters to specify training algorithm
+    """
+    def __init__(self, 
+                 layers=(10,),
+                 input_layer=-1,
+                 output_layer=-1,
+                 hidden_activation='logistic',
+                 output_activation='linear',
+                 random_state=42,
+                 input_noise=0,
+                 hidden_noise=0,
+                 input_dropouts=0,
+                 hidden_dropouts=0,
+                 decode_from=1,
+                 scaler=MinMaxScaler(),
+                 features=None,
+                 trainers=None):
+        TheanetsBase.__init__(self, layers=layers,
+                               input_layer=input_layer,
+                               output_layer=output_layer,
+                               hidden_activation=hidden_activation,
+                               output_activation=output_activation,
+                               random_state=random_state,
+                               input_noise=input_noise,
+                               hidden_noise=hidden_noise,
+                               input_dropouts=input_dropouts,
+                               hidden_dropouts=hidden_dropouts,
+                               decode_from=decode_from,
+                               scaler=scaler,
+                               trainers=trainers)
+        Classifier.__init__(self, features=features)
 
     def partial_fit(self, X, y, sample_weight=None, new_trainer=True,  **trainer):
         """
@@ -236,4 +290,91 @@ class TheanetsClassifier(Classifier):
         :param pandas.DataFrame X: data shape [n_samples, n_features]
         :return: iterator
         """
-        raise NotImplementedError('staged_predict_proba is not supported for theanets')
+        raise NotImplementedError('staged_predict_proba is not supported for theanets classifier')
+
+
+class TheanetsRegressor(TheanetsBase, Regressor):
+
+    def __init__(self,
+                 layers=(10,),
+                 input_layer=-1,
+                 output_layer=-1,
+                 hidden_activation='logistic',
+                 output_activation='linear',
+                 random_state=42,
+                 input_noise=0,
+                 hidden_noise=0,
+                 input_dropouts=0,
+                 hidden_dropouts=0,
+                 decode_from=1,
+                 scaler=MinMaxScaler(),
+                 features=None,
+                 trainers=None):
+        TheanetsBase.__init__(self,
+                              layers=layers,
+                              input_layer=input_layer,
+                              output_layer=output_layer,
+                              hidden_activation=hidden_activation,
+                              output_activation=output_activation,
+                               random_state=random_state,
+                               input_noise=input_noise,
+                               hidden_noise=hidden_noise,
+                               input_dropouts=input_dropouts,
+                               hidden_dropouts=hidden_dropouts,
+                               decode_from=decode_from,
+                               scaler=scaler,
+                               trainers=trainers)
+        Regressor.__init__(self, features=features)
+
+    def partial_fit(self, X, y, sample_weight=None, new_trainer=True,  **trainer):
+        """
+        Train the regressor by training the existing regressor again.
+
+        :param pandas.DataFrame X: data shape [n_samples, n_features]
+        :param y: values - array-like of shape [n_samples]
+        :param sample_weight: weight of events,
+               array-like of shape [n_samples] or None if all weights are equal
+        :param bool new_trainer: True if the trainer is not stored in self.trainers
+        :param dict trainer: parameters of the training algorithm we want to use now
+        :return: self
+        """
+        if sample_weight is not None:
+            # https://github.com/lmjohns3/theanets/issues/58
+            raise NotImplementedError('sample_weight is not supported for theanets')
+        X, y, sample_weight = check_inputs(X, y, sample_weight)
+        X = self._transform_data(X)
+        if self.exp is None:
+            # initialize experiment
+            if self.input_layer == -1:
+                self.input_layer = X.shape[1]
+            if self.output_layer == -1:
+                self.output_layer = 1
+            layers = [self.input_layer] + self.layers + [self.output_layer]
+            print(layers)
+            self.exp = tnt.Experiment(tnt.Regressor, layers=layers, rng=self._get_rng(), **self.network_params)
+        if new_trainer:
+            self.trainers.append(trainer)
+        numpy.random.seed(42)
+        self.exp.train((X.astype(numpy.float32), y.astype(numpy.int32)),
+                       **trainer)
+        return self
+
+    def predict(self, X):
+        """
+        Predict probabilities
+
+        :param pandas.DataFrame X: data shape [n_samples, n_features]
+        :rtype: numpy.array of shape [n_samples, n_classes] with probabilities
+        """
+        assert self._is_fitted(), 'Regressor wasn`t fitted, please call `fit` first'
+        X = self._transform_data(X)
+        return self.exp.network.predict(X.astype(numpy.float32))
+
+    def staged_predict(self, X):
+        """
+        Predicts values on each stage
+
+        :param pandas.DataFrame X: data shape [n_samples, n_features]
+        :return: iterator
+        """
+        raise NotImplementedError('staged_predict is not supported for theanets regressor')
