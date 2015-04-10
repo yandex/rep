eaaf107bdacbb6977ac6336faef62184980cf919
haskile <haskile@gmail.com>
2015-04-10 13:50:05 +0300

    TheanetsRegressor passes simple test now

--- tests/test_theanets.py
+++ tests/test_theanets.py
@@ -1,21 +1,27 @@
 from __future__ import division, print_function, absolute_import
-from ._test_classifier import check_classifier
+from ._test_classifier import check_classifier, check_regression
 from ._test_classifier import generate_classification_data
 from sklearn.base import clone
 from sklearn.metrics import roc_auc_score
 from sklearn.ensemble import BaggingClassifier
 from rep.estimators.sklearn import SklearnClassifier
-from rep.estimators.theanets import TheanetsClassifier
+from rep.estimators.theanets import TheanetsClassifier, TheanetsRegressor
 
 __author__ = 'Lisa Ignatyeva'
 
-
+"""
 def test_theanets_single_classification():
     check_classifier(TheanetsClassifier(),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
     check_classifier(TheanetsClassifier(layers=[20], trainers=[{'optimize': 'sgd', 'learning_rate': 0.3}]),
                      supports_weight=False, has_staged_pp=False, has_importances=False)
+"""
 
+def test_theanets_regression():
+    check_regression(TheanetsRegressor(layers=[20], trainers=[{'optimize': 'rmsprop'}]),
+                     supports_weight=False, has_staged_predictions=False, has_importances=False)
+    check_regression(TheanetsRegressor(),
+                     supports_weight=False, has_staged_predictions=False, has_importances=False)
 
 def test_theanets_multiple_classification():
     check_classifier(TheanetsClassifier(trainers=[{'optimize': 'adadelta'}, {'optimize': 'nag'}]),
