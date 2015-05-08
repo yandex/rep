61d3b2a5b0996952fe0bc9b732e455274fd925f6
quadrocube <gliderok@gmail.com>
2015-05-08 13:24:15 +0300

    added more docs

--- rep/estimators/neurolab.py
+++ rep/estimators/neurolab.py
@@ -100,9 +100,11 @@ class NeurolabBase(object):
     def _fit(self, X, y, y_train):
         x_train = self._transform_input(self._get_train_features(X), y)
 
+        # Prepare parameters depending on network purpose (classification \ regression)
         net_params = self._prepare_params_specific(self.net_params, x_train)
 
-        net = self._prepare_net(**net_params)
+        init = self._get_initializers(self.net_type)
+        net = init(net_params)
 
         # To allow similar initf function on all layers
         initf_iterable = self.initf if hasattr(self.initf, '__iter__') else [self.initf]*len(net.layers)
@@ -133,10 +135,6 @@ class NeurolabBase(object):
             self.scaler.fit(X, y)
         return scipy.special.expit(self.scaler.transform(X) / 3)
 
-    def _prepare_net(self, **net_params):
-        init = self._get_initializers(self.net_type)
-        return init(**net_params)
-
     @staticmethod
     def _prepare_params_general(net_params, x_train, out_layer_size):
         params = deepcopy(net_params)
@@ -153,6 +151,7 @@ class NeurolabBase(object):
         if 'cn' in params:
             params['cn'] = out_layer_size
 
+        # Set output layer size
         if 'size' in params:
             params['size'] += [out_layer_size]
 
@@ -180,7 +179,10 @@ class NeurolabRegressor(NeurolabBase, Regressor):
     :type initf: anything implementing call(layers). e.g. nl.init.* or list[nl.init.*] of shape [n_layers]
     :param trainf: net train function
     :param scaler: transformer to apply to the input objects
-    :param dict kwargs: additional arguments to net __init__
+    :type scaler: sklearn-like scaler or False (do not scale features -- use with care and keep track of minmax param)
+    :param list layers: list of numbers denoting size of each hidden layer
+    :param dict kwargs: additional arguments to net __init__, varies with different net_types
+                        See: https://pythonhosted.org/neurolab/lib.html
     """
 
     def __init__(self, net_type='feed-forward',
@@ -193,6 +195,13 @@ class NeurolabRegressor(NeurolabBase, Regressor):
         NeurolabBase.__init__(self, net_type=net_type, initf=initf, trainf=trainf, scaler=scaler, **kwargs)
 
     def fit(self, X, y):
+        """
+        Fit model on data
+
+        :param X: pandas.DataFrame
+        :param y: iterable denoting corresponding value in object
+        :return: self
+        """
         # TODO Some networks do not support regression?
         X, y, _ = check_inputs(X, y, None)
         y_train = y.reshape(len(y), 1)
@@ -237,7 +246,10 @@ class NeurolabClassifier(NeurolabBase, Classifier):
     :type initf: anything implementing call(layers). e.g. nl.init.* or list[nl.init.*] of shape [n_layers]
     :param trainf: net train function
     :param scaler: transformer to apply to the input objects
-    :param dict kwargs: additional arguments to net __init__
+    :type scaler: sklearn-like scaler or False (do not scale features -- use with care and keep track of minmax param)
+    :param list layers: list of numbers denoting size of each hidden layer
+    :param dict kwargs: additional arguments to net __init__, varies with different net_types
+                        See: https://pythonhosted.org/neurolab/lib.html
     """
     def __init__(self, net_type='feed-forward',
                  features=None,
@@ -250,6 +262,13 @@ class NeurolabClassifier(NeurolabBase, Classifier):
         self.classes_ = None
 
     def fit(self, X, y):
+        """
+        Fit model on data
+
+        :param X: pandas.DataFrame
+        :param y: iterable denoting corresponding object classes
+        :return: self
+        """
         # Some networks do not support classification
         assert self.net_type not in CANT_CLASSIFY, 'Network type does not support classification'
         X, y, _ = check_inputs(X, y, None)
@@ -282,7 +301,7 @@ class NeurolabClassifier(NeurolabBase, Classifier):
         return np.array(OneHotEncoder(n_values=len(np.unique(y))).fit_transform(y.reshape((len(y), 1))).todense())
 
     def _get_classes(self, y, y_transformed):
-        # I found no proved evidence that OHE is guaranteed to encode values in accordance to their order
+        # I've found no direct evidence that OHE is guaranteed to encode values in accordance to their order
         # So this hack appeared
         cl = np.zeros(len(np.unique(y)))
         for true_cl, enc in zip(y, y_transformed):
@@ -292,11 +311,12 @@ class NeurolabClassifier(NeurolabBase, Classifier):
     def _prepare_params_specific(self, params, x_train):
         net_params = NeurolabBase._prepare_params_general(params, x_train, len(self.classes_))
 
-        # Classification networks should have SoftMax as the transfer function on output layer
+        # Default parameters for transfer functions in classifier networks
         if 'transf' not in net_params:
             net_params['transf'] = \
                 [nl.trans.TanSig()] * len(net_params['size']) if 'size' in net_params else nl.trans.SoftMax()
 
+        # Classification networks should have SoftMax as the transfer function on output layer
         if hasattr(net_params['transf'], '__iter__'):
             net_params['transf'][-1] = nl.trans.SoftMax()
         else:
