d1f8d12479bad743b560eccbfd11980326677d5b
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-05-20 02:03:52 +0300

    split docker image in two

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@
 NOTEBOOK_DIR='/notebooks'
 [ -d '/logdir' ] && LOG_DIR='/logdir'
 
-ipython notebook --ip=* --port=8080 --pylab inline --no-browser --notebook-dir=$NOTEBOOK_DIR &>> $LOG_DIR/notebook.log
+OPTIONS="--port=8080 --no-browser"
+[ -n "$*" ] && OPTIONS=$*
+
+ipython notebook $OPTIONS --ip=* --notebook-dir=$NOTEBOOK_DIR &>> $LOG_DIR/notebook.log
