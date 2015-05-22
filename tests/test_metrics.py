2afb8e23b320bb5f6cba301a0e3951e764cac8e0
tata-antares <tata.antares@yandex.ru>
2015-05-22 16:00:26 +0300

    add metrics fpr with fixed tpr and tpr with fixed fpr.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -58,3 +58,33 @@ def test_roc_auc(size=1000):
     print(value, value2)
 
     assert numpy.allclose(value, value2)
+
+
+def fpr_tpr(size, prediction):
+    from sklearn.metrics import roc_curve
+    random_labels = numpy.random.choice(2, size=size)
+
+    proba = numpy.ndarray((len(prediction), 2))
+    proba[:, 0] = 1 - prediction
+    proba[:, 1] = prediction
+    sample_weight = numpy.random.random(size=size)
+
+    threshold = 0.75
+    loss_fpr, loss_tpr = metrics.FPRatTPR(threshold), metrics.TPRatFPR(threshold)
+    fprs, tprs, _ = roc_curve(random_labels, prediction, sample_weight=sample_weight)
+    value_fpr = loss_fpr(random_labels, proba, sample_weight=sample_weight)
+    value_tpr = loss_tpr(random_labels, proba, sample_weight=sample_weight)
+    value_fpr2 = fprs[numpy.searchsorted(tprs, threshold)]
+    value_tpr2 = tprs[numpy.searchsorted(fprs, threshold) - 1]
+    print(value_fpr, value_fpr2)
+    print(value_tpr, value_tpr2)
+
+    assert numpy.allclose(value_fpr, value_fpr2, atol=1e-3)
+    assert numpy.allclose(value_tpr, value_tpr2, atol=1e-3)
+
+
+def test_fpr_tpr(size=10000):
+    prediction = numpy.random.permutation(size)
+    fpr_tpr(size, prediction)
+    prediction = numpy.ones(size)
+    fpr_tpr(size, prediction)
