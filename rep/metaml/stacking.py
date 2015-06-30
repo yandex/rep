4422b2b4e026d34c5f6cdfc202cb6787f020edd5
tata-antares <tata.antares@yandex.ru>
2015-06-30 14:54:48 +0300

    refactor of stacking: fix error with complex features and complex split feature

--- rep/metaml/stacking.py
+++ rep/metaml/stacking.py
@@ -7,7 +7,7 @@ import numpy
 from sklearn.base import clone
 
 from ..estimators import Classifier
-from ..estimators.utils import check_inputs
+from ..estimators.utils import check_inputs, _get_features
 
 
 __author__ = 'Alex Rogozhnikov'
@@ -24,34 +24,55 @@ class FeatureSplitter(Classifier):
     :param str split_feature: the name of key feature,
     :param base_estimator: the classifier, its' copies are trained on parts of dataset
     :param list[str] features: list of columns classifier uses
+
+    .. note:: `split_feature` must be in list of `features`
     """
-    def __init__(self, split_feature, base_estimator, features=None):
+    def __init__(self, split_feature, base_estimator, train_features=None):
         self.base_estimator = base_estimator
         self.split_feature = split_feature
-        Classifier.__init__(self, features=features)
+        self.train_features = train_features
+        Classifier.__init__(self, features=self._features())
+
+    def _features(self):
+        if self.train_features is None:
+            return None
+        else:
+            return list(self.train_features) + [self.split_feature]
+
+    def _get_features(self, X, allow_nans=False):
+        """
+        :param pandas.DataFrame X: train dataset
+
+        :return: pandas.DataFrame with used features
+        """
+        split_column_values, _ = _get_features([self.split_feature], X, allow_nans=allow_nans)
+        split_column_values = numpy.ravel(numpy.array(split_column_values))
+        X_prepared, self.train_features = _get_features(self.train_features, X, allow_nans=allow_nans)
+        self.features = self._features()
+        return split_column_values, X_prepared
 
     def fit(self, X, y, sample_weight=None):
         """
         Fit dataset.
+
         :param X: pandas.DataFrame of shape [n_samples, n_features] with features
         :param y: array-like of shape [n_samples] with targets
         :param sample_weight: array-like of shape [n_samples] with events weights or None.
-        :return: self, trained classifier.
+
+        :return: self
         """
-        # Not taking training features - they should be passed to children classifier
-        X, y, sample_weight = check_inputs(X, y, sample_weight=sample_weight, allow_none_weights=False)
-        X = self._get_train_features(X)
-        self._set_classes(y)
+        X, y, sample_weight = check_inputs(X, y, sample_weight=sample_weight, allow_none_weights=True)
         # TODO cover the case of missing labels in subsets.
-        split_column_values = numpy.array(X[self.split_feature])
+        split_column_values, X = self._get_features(X)
+        self._set_classes(y)
         self.base_estimators = {}
         for value in numpy.unique(split_column_values):
             rows = numpy.array(split_column_values) == value
             base_classifier = clone(self.base_estimator)
             if sample_weight is None:
-                base_classifier.fit(X.loc[rows, :], y[rows])
+                base_classifier.fit(X.iloc[rows, :], y[rows])
             else:
-                base_classifier.fit(X.loc[rows, :], y[rows], sample_weight=sample_weight[rows])
+                base_classifier.fit(X.iloc[rows, :], y[rows], sample_weight=sample_weight[rows])
             self.base_estimators[value] = base_classifier
         return self
 
@@ -63,9 +84,10 @@ class FeatureSplitter(Classifier):
         :param X: pandas.DataFrame of shape [n_samples, n_features]
         :return: probabilities of shape [n_samples, n_classes]
         """
+        split_column_values, X = self._get_features(X)
         result = numpy.zeros([len(X), self.n_classes_])
         for value, estimator in self.base_estimators.items():
-            mask = numpy.array(X[self.split_feature]) == value
+            mask = split_column_values == value
             result[mask, :] = estimator.predict_proba(X.loc[mask, :])
         return result
 
@@ -78,10 +100,11 @@ class FeatureSplitter(Classifier):
         :param X: pandas.DataFrame of shape [n_samples, n_features]
         :return: iterable sequence of numpy.arrays of shape [n_samples, n_classes]
         """
+        split_column_values, X = self._get_features(X)
         result = numpy.zeros([len(X), self.n_classes_])
         masks_iterators = []
         for value, estimator in self.base_estimators.items():
-            mask = numpy.array(X[self.split_feature]) == value
+            mask = split_column_values == value
             prediction_iterator = estimator.staged_predict_proba(X.loc[mask, :])
             masks_iterators.append([mask, prediction_iterator])
         try:
