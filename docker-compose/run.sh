9b39411f105faa7d524f09273709a519bb52e66e
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-07-15 23:32:57 +0200

    path fix

--- docker-compose/run.sh
+++ docker-compose/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+halt() {
+echo $*
+exit 1
+}
+
+
 HERE=`dirname $0`
 CIDFILE=$HERE/rep.cid
+[ ! -d "$HERE/notebooks" ] && halt "Are you running from the instance directory? (run \`./init.sh INSTANCE_DIR\`)"
 [ -f $CIDFILE ] && rm $CIDFILE
 cd $HERE
 docker-compose up -d && echo "$(basename `pwd -L`)_rep_1" > $CIDFILE
--- docker-compose/stop.sh
+++ docker-compose/stop.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+halt() {
+echo $*
+exit 1
+}
+
+
 HERE=`dirname $0`
 CIDFILE=$HERE/rep.cid
+[ ! -d $HERE/notebooks ] && halt "Are you running from the instance directory?"
 cd $HERE
 docker-compose stop
 [ -f $CIDFILE ] && rm -f $CIDFILE
