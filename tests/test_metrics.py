7558d353a226006e7f80ed0c937adacb040cad86
Alexey <iamfullofspam@gmail.com>
2015-08-15 18:08:01 +0300

    added test of optimal metric

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,29 +2,12 @@ from __future__ import division, print_function, absolute_import
 
 import numpy
 from rep.report import metrics
+from rep.report.metrics import OptimalAMS, OptimalSignificance, significance, ams
 
 __author__ = 'Alex Rogozhnikov'
 
 
-def test_optimal_metrics(size=1000):
-    prediction = numpy.random.random(size=size)
-    random_labels = numpy.random.choice(2, size=size)
-
-    def ams_like(s, b):
-        return s / (b + 1. / 100. / size)
-
-    # setting 'the best event' to be signal
-    random_labels[numpy.argmax(prediction)] = 1
-    optimal_ams = metrics.OptimalMetric(ams_like)
-    proba = numpy.ndarray((len(prediction), 2))
-    proba[:, 0] = 1 - prediction
-    proba[:, 1] = prediction
-    score = optimal_ams(random_labels, proba)
-
-    assert score >= 100
-
-
-def test_optimal_metricsndim(size=1000):
+def test_optimal_metrics_ndim(size=1000):
     prediction = numpy.random.random(size=size)
     pid = numpy.random.random(size=size)
     random_labels = numpy.random.choice(2, size=size)
@@ -110,3 +93,44 @@ def test_fpr_tpr(size=10000):
     fpr_tpr(size, prediction)
     prediction = numpy.ones(size)
     fpr_tpr(size, prediction)
+
+
+def test_optimal_metrics(size=1000):
+    prediction = numpy.random.random(size=size)
+    random_labels = numpy.random.choice(2, size=size)
+
+    def ams_like(s, b):
+        return s / (b + 1. / 100. / size)
+
+    # setting 'the best event' to be signal
+    random_labels[numpy.argmax(prediction)] = 1
+    optimal_ams = metrics.OptimalMetric(ams_like)
+    proba = numpy.ndarray((len(prediction), 2))
+    proba[:, 0] = 1 - prediction
+    proba[:, 1] = prediction
+    score = optimal_ams(random_labels, proba)
+
+    assert score >= 100
+
+
+def test_optimal_metric_function(size=10000):
+    labels = numpy.random.randint(0, 2, size=size)
+    predictions = numpy.random.random(size=[size, 2])
+    predictions /= predictions.sum(axis=1, keepdims=True)
+    sample_weight = numpy.random.random(size=size)
+
+    for metric, optimal_metric in [(significance, OptimalSignificance()),
+                                   (ams, OptimalAMS())]:
+        optimal_metric.fit(None, labels, sample_weight=sample_weight)
+        value = optimal_metric(labels, predictions, sample_weight=sample_weight)
+        thresholds, values = optimal_metric.compute(labels, predictions, sample_weight=sample_weight)
+        assert numpy.max(values) == value, "maximal value doesn't coincide"
+        index = numpy.random.randint(0, len(thresholds))
+        threshold = thresholds[index]
+        passed = numpy.array(predictions[:, 1] >= threshold)
+
+        s = optimal_metric.expected_s * numpy.average(passed, weights=sample_weight * (labels == 1))
+        b = optimal_metric.expected_b * numpy.average(passed, weights=sample_weight * (labels == 0))
+        assert numpy.allclose(metric(s, b), values[index]), 'no coincidence {} {} {}'.format(type(optimal_metric),
+                                                                                             metric(s, b),
+                                                                                             values[index])
