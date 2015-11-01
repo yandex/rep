a3212714b29906a2ab0dbc11dce85e1d7bf8e001
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-11-02 02:36:27 +0300

    conda environment file

--- install-conda-env.sh
+++ install-conda-env.sh
@@ -16,8 +16,7 @@ apt-get install -y  \
     gfortran \
     git \
     libxft-dev \
-    libxpm-dev
-
+	libxpm-dev
 
 mkdir $HOME/.config/matplotlib -p
 echo 'backend: agg' > $HOME/.config/matplotlib/matplotlibrc
@@ -26,25 +25,25 @@ chmod +x miniconda.sh
 ./miniconda.sh -b && rm ./miniconda.sh || halt "Error installing miniconda"
 export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes conda
-conda install --yes python=2.7
-conda install --yes -q -c remenska root_numpy
+conda create --file environment_py2.yaml
+conda uninstall --yes gcc qt
+conda clean -p -t
+
 source $HOME/miniconda/bin/thisroot.sh
 [ -n "$ROOTSYS" ] || halt "Error installing ROOT"
-python -c 'import ROOT'
-python -c 'import root_numpy' || halt "Error installing root_numpy"
-conda install --yes pip numpy scipy six pandas setuptools
-conda install --yes theano scikit-learn matplotlib ipython-notebook
-conda uninstall --yes gcc qt 
-conda clean -p -t
- # install xgboost
+python -c 'import ROOT, root_numpy' || halt "Error installing root_numpy"
+
+# install xgboost
 git clone https://github.com/dmlc/xgboost.git
 cd xgboost
- # taking particular xgboost commit, which is working
+# taking particular xgboost commit, which is working
 git checkout 8e4dc4336849c24ae48636ae60f5faddbb789038
 ./build.sh
 cd python-package
 python setup.py install
 cd ../..
- # end install xgboost
+# end install xgboost
 python -c 'import xgboost' || halt "Error installing XGboost"
-pip install -q neurolab==0.3.5 theanets==0.6.2 pybrain==0.3 || halt "Error installing ANN packages"
+
+echo 'source activate py2' >> $HOME/.bashrc
+echo 'source $ENV_BIN_DIR/thisroot.sh' >> $HOME/.bashrc
