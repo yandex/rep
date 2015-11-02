c8eede7086b651983fa71575eb34040349882a42
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-11-03 01:21:46 +0300

    one more time

--- ci/install-conda-env.sh
+++ ci/install-conda-env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 halt() {
   echo $*
@@ -7,27 +7,32 @@ halt() {
 
 HERE=`dirname $0`
 [ -z "$HOME" ] && export HOME="/root"
-apt-get update
-apt-get install -y  \
-    build-essential \
-    libatlas-base-dev \
-    liblapack-dev \
-    libffi-dev \
-    wget \
-    gfortran \
-    git \
-    libxft-dev \
-	libxpm-dev
+# apt-get update
+# apt-get install -y  \
+#     build-essential \
+#     libatlas-base-dev \
+#     liblapack-dev \
+#     libffi-dev \
+#     wget \
+#     gfortran \
+#     git \
+#     libxft-dev \
+# 	libxpm-dev
 
 mkdir $HOME/.config/matplotlib -p
 echo 'backend: agg' > $HOME/.config/matplotlib/matplotlibrc
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
-export PATH+=:$HOME/miniconda/bin
+if [ -n "$TRAVIS_PYTHON_VERSION" ] ; then
+    PENV_NAME="py${TRAVIS_PYTHON_VERSION:0:1}"
+else
+    PENV_NAME=`python --version 2>&1|awk '{print $2}'|awk -F . '{print $1}'`
+    PENV_NAME="py${PVERSION}"
+fi
+export PATH=$HOME/miniconda/bin:$PATH
 ./miniconda.sh -b && rm ./miniconda.sh || halt "Error installing miniconda"
-# conda update --yes conda
-conda env create --file $HERE/environment_py2.yaml || halt "Error installing py2 environment"
-source activate py2
+conda env create --file $HERE/environment_${PENV_NAME}.yaml || halt "Error installing $PENV_NAME environment"
+source activate $PENV_NAME
 conda uninstall --yes gcc qt
 conda clean --yes -p -t
 
@@ -48,8 +53,8 @@ python -c 'import ROOT, root_numpy' || halt "Error installing root_numpy"
 python -c 'import xgboost' || halt "Error installing XGboost"
 
 # environment
-cat << EOF > $HOME/.bashrc
-export PATH=$HOME/miniconda/bin:$PATH
-source activate py2
-source $ENV_BIN_DIR/thisroot.sh
-EOF
+# cat << EOF > $HOME/.bashrc
+# export PATH=$HOME/miniconda/bin:$PATH
+# source activate py2
+# source $ENV_BIN_DIR/thisroot.sh
+# EOF
