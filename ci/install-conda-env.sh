4431842d40e42a8d7b1e97f80a95945ef853ae7e
Andrey Ustyuzhanin <andrey.u@gmail.com>
2015-11-03 16:47:23 +0300

    added Linux-specific env

--- ci/install-conda-env.sh
+++ ci/install-conda-env.sh
@@ -8,9 +8,10 @@ halt() {
 }
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SYSTEM=`uname -s`
 
 [ -z "$HOME" ] && export HOME="/root"
-if [ `uname -s` == "Linux" ] && which apt-get > /dev/null ; then
+if [ $SYSTEM == "Linux" ] && which apt-get > /dev/null ; then
     sudo apt-get update
     sudo apt-get install -y  \
         build-essential \
@@ -38,10 +39,12 @@ if ! which conda ; then
     ./miniconda.sh -b && rm ./miniconda.sh || halt "Error installing miniconda"
     export PATH=$HOME/miniconda/bin:$PATH
 fi
-conda env create --name $PENV_NAME --file $HERE/environment.yaml #|| halt "Error installing $PENV_NAME environment"
+ENV_FILE=$HERE/environment.yaml
+[ -f $HERE/environment_${SYSTEM}.yaml ] && ENV_FILE=environment_${SYSTEM}.yaml
+conda env create --name $PENV_NAME --file $ENV_FILE #|| halt "Error installing $PENV_NAME environment"
 source activate $PENV_NAME
 conda uninstall --yes gcc qt
-conda clean --yes -p
+conda clean --yes -pt
 
 # install xgboost
 git clone https://github.com/dmlc/xgboost.git
