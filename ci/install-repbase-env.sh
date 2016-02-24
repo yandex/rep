add80e3f0cd64dadce5da874ff5f4023f9ce7922
Alexey <iamfullofspam@gmail.com>
2016-02-24 17:12:22 +0300

    cleaned travis.yml, install-repbase-env.sh and setup.py

--- ci/install-repbase-env.sh
+++ ci/install-repbase-env.sh
@@ -10,16 +10,17 @@ function halt {
 PYTHON_MAJOR_VERSION=2
 [ "$1" == "-h" ] && halt "Usage: $0 [PYTHON_MAJOR_VERSION=2]\ne.g.: $0 3"
 [ -n "$1" ] && PYTHON_MAJOR_VERSION=$1 && shift
+# when testing on travis, we use travis variable
 if [ -n "$TRAVIS_PYTHON_VERSION" ] ; then
     PYTHON_MAJOR_VERSION=${TRAVIS_PYTHON_VERSION:0:1}
 fi
 REP_ENV_NAME="rep_py${PYTHON_MAJOR_VERSION}"
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SYSTEM=`uname -s`
+SYSTEM=$(uname -s)
 
 [ -z "$HOME" ] && export HOME="/root"
-if [ $SYSTEM == "Linux" ] && which apt-get > /dev/null && ! dpkg -l |grep libxpm-dev > /dev/null ; then
+if [ $SYSTEM == "Linux" ] && which apt-get > /dev/null && ! dpkg -l | grep libxpm-dev > /dev/null ; then
     sudo apt-get update
     sudo apt-get install -y  \
         build-essential \
@@ -68,6 +69,7 @@ echo "Create env jupyterhub_py3"
 conda env create -q --name jupyterhub_py3 --file $JUPYTERHUB_ENV_FILE > /dev/null
 source activate $REP_ENV_NAME || halt "Error installing $REP_ENV_NAME environment"
 
+# TODO do we really need this?
 if [ "$PYTHON_MAJOR_VERSION" == "3" ] ; then
   # fix for root_numpy 4.3.0 from remenska
   echo $PYTHONPATH
@@ -83,15 +85,14 @@ conda uninstall --yes -q gcc qt
 conda clean --yes -s -p -l -i -t
 
 # test installed packages
-pushd $ENV_BIN_DIR/.. 
-source 'bin/thisroot.sh' || halt "Error installing ROOT"
-popd
+source "${ENV_BIN_DIR}/../bin/thisroot.sh" || halt "Error installing ROOT"
 python -c 'import ROOT, root_numpy' || halt "Error installing root_numpy"
-python -c 'import xgboost' || halt "Error installing XGboost"
+python -c 'import xgboost' || halt "Error installing XGBoost"
+
 # printing message about environment
 cat <<- EOF
     # add to your environment:
-    export PATH=$HOME/miniconda/bin:$PATH
-    source activate $REP_ENV_NAME
-    pushd $ENV_BIN_DIR/.. ; source 'bin/thisroot.sh' ; popd
+    export PATH=\$HOME/miniconda/bin:\$PATH
+    source activate \$REP_ENV_NAME
+    source \$ENV_BIN_DIR/../bin/thisroot.sh
 EOF
