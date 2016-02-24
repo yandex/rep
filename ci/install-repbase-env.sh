32ccc0483674e1559acc72c8300b557df5414083
Alexey <iamfullofspam@gmail.com>
2016-02-24 23:48:08 +0300

    not deleting conda packages after installation anymore

--- ci/install-repbase-env.sh
+++ ci/install-repbase-env.sh
@@ -41,13 +41,16 @@ fi
 # changing matplotlib configuration file to avoid this
 mkdir -p $HOME/.config/matplotlib && echo 'backend: agg' > $HOME/.config/matplotlib/matplotlibrc
 
-# exit existing envirnoments
+# exit existing environments
 [ -n "$VIRTUAL_ENV" ] && deactivate
 [ -n "$CONDA_ENV_PATH" ] && source deactivate
 
 if ! which conda ; then
-    MINICONDA_FILE="Miniconda-latest-Linux-x86_64.sh"
-    [ "$PYTHON_MAJOR_VERSION" == "3" ] && MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
+    if [ "$PYTHON_MAJOR_VERSION" == "3" ]; then
+        MINICONDA_FILE="Miniconda-latest-Linux-x86_64.sh"
+    else
+        MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
+    fi
     wget http://repo.continuum.io/miniconda/$MINICONDA_FILE -O miniconda.sh
     chmod +x miniconda.sh
     ./miniconda.sh -b -p $HOME/miniconda || halt "Error installing miniconda"
@@ -59,7 +62,7 @@ fi
 
 REP_ENV_FILE="$HERE/environment-rep.yaml"
 JUPYTERHUB_ENV_FILE="$HERE/environment-jupyterhub.yaml"
-echo "Create env $REP_ENV_NAME"
+echo "Create conda venv $REP_ENV_NAME"
 conda env create -q --name $REP_ENV_NAME --file $REP_ENV_FILE > /dev/null
 source activate $REP_ENV_NAME || halt "Error installing $REP_ENV_NAME environment"
 
@@ -75,11 +78,12 @@ if [ "$PYTHON_MAJOR_VERSION" == "3" ] ; then
   [ -d $EGG ] && [ ! -f $PATH_FILE ] && pushd $SITES_PACKAGES && echo "./root_numpy-"* > $PATH_FILE && popd
 fi
 
+echo 'Removing conda packages and caches'
 conda uninstall --yes -q gcc qt
-conda clean --yes -s -p -l -i -t
+conda clean --yes -s -l -i -t
 
 # test installed packages
-source "${ENV_BIN_DIR}/../bin/thisroot.sh" || halt "Error installing ROOT"
+source "${ENV_BIN_DIR}/thisroot.sh" || halt "Error installing ROOT"
 python -c 'import ROOT, root_numpy' || halt "Error installing root_numpy"
 python -c 'import xgboost' || halt "Error installing XGBoost"
 
@@ -88,5 +92,5 @@ cat <<- EOF
     # add to your environment:
     export PATH=\$HOME/miniconda/bin:\$PATH
     source activate \$REP_ENV_NAME
-    source \$ENV_BIN_DIR/../bin/thisroot.sh
+    source \$ENV_BIN_DIR/thisroot.sh
 EOF
